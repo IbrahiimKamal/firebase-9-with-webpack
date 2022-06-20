0b5d0923cf8b3fcbb185312a80cce7bbea6394b1
IbrahiimKamal <ebrahiiim.kamal@gmail.com>
2022-06-20 11:46:32 +0200

    initialized firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEdGwwq4mUeEynolZloNNiEFPy49__n80',
+  authDomain: 'fir-9-aadb2.firebaseapp.com',
+  projectId: 'fir-9-aadb2',
+  storageBucket: 'fir-9-aadb2.appspot.com',
+  messagingSenderId: '493373763953',
+  appId: '1:493373763953:web:590fa40fa47e345b6f46d0',
+};
+
+initializeApp(firebaseConfig);
